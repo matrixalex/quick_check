326db8f204ca6fc63d0ec2ac9b6e1f4c09143433
matrixalex <matrixalex96@gmail.com>
2020-12-10 01:19:19 +0300

    Заготовки под главные страницы пользователей по типу учетной записи

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from src.apps.users.models.user_type import SYSTEM_ADMIN, ADMIN, TEACHER, PUPIL
 
 
 def index(request):
@@ -12,9 +13,48 @@ def index(request):
         return redirect('/auth/login')
     if user.is_superuser:
         return redirect('/admin')
-    return render(request, 'index.html', context={'user': request.user})
+    if not user.status.id == SYSTEM_ADMIN:
+        return system_admin_page(request)
+    elif user.status.id == ADMIN:
+        return admin_page(request)
+    elif user.status.id == TEACHER:
+        return teacher_page(request)
+    else:
+        return pupil_page(request)
 
 
 def not_allowed(request):
     """Рендер страницы Нет доступа"""
     return render(request, 'not_allowed.html')
+
+
+def system_admin_page(request):
+    user = request.user
+    data = {'user': user}
+    if not user.status.id == SYSTEM_ADMIN:
+        return redirect('/not-allowed')
+    return render(request, 'index.html', context=data)
+
+
+def admin_page(request):
+    user = request.user
+    data = {'user': user}
+    if not user.status.id == ADMIN:
+        return redirect('/not-allowed')
+    return render(request, 'index.html', context=data)
+
+
+def teacher_page(request):
+    user = request.user
+    data = {'user': user}
+    if not user.status.id == TEACHER:
+        return redirect('/not-allowed')
+    return render(request, 'index.html', context=data)
+
+
+def pupil_page(request):
+    user = request.user
+    data = {'user': user}
+    if not user.status.id == PUPIL:
+        return redirect('/not-allowed')
+    return render(request, 'index.html', context=data)
