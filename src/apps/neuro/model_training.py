53dea66c3963307349f9a42243f5227c953ae20e
matrixalex <matrixalex96@gmail.com>
2021-01-15 22:58:07 +0300

    Новая сетка при работе нейронки

--- src/apps/neuro/model_training.py
+++ src/apps/neuro/model_training.py
@@ -9,26 +9,26 @@ from torch.nn.functional import sigmoid
 class mach1(nn.Module):
     def __init__(self):
         super(mach1, self).__init__()
-        self.conv0 = nn.Conv2d(3, 28, 3)
-        self.fc0 = nn.ReLU()
-        self.conv1 = nn.Conv2d(28, 14, 3)
-        self.pool1 = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(14*12*12,136)
-        self.fc2 = nn.Linear(136,101)
-        self.fc3 = nn.Linear(101,41)
-        self.fc4 = nn.LogSoftmax(dim=1)
-
+        self.conv0 = nn.Conv2d(3, 96, 11, stride=4)
+        self.pool0 = nn.MaxPool2d(3, stride=2)
+        self.conv1 = nn.Conv2d(96,256,5,padding=2)
+        self.pool1 = nn.MaxPool2d(3, stride=2, padding=1)
+        self.conv2 = nn.Conv2d(256,384,3,padding=1)
+        self.conv3 = nn.Conv2d(384,384,3,padding=1)
+        self.pool2 = nn.MaxPool2d(3, stride=2)
+        self.fc0 = nn.Linear(384*2*2, 16)
+        self.fc1 = nn.LogSoftmax(dim=1)
     def forward(self, x):
         x = self.conv0(x)
-        x = self.fc0(x)
+        x = self.pool0(x)
         x = self.conv1(x)
         x = self.pool1(x)
-        x = x.view(-1,14*12*12)
-        x = sigmoid(self.fc1(x))
-        x = sigmoid(self.fc2(x))
-        x = sigmoid(self.fc3(x))
-        x = self.fc4(x)
-        return x
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.pool2(x)
+        x = x.view(-1,384*2*2)
+        x = self.fc0(x)
+        return self.fc1(x)
 
 
 ml_model = None
