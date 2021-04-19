972d817670a6fc6c834b20c0777861daa0673ebd
matrixalex <matrixalex96@gmail.com>
2021-04-19 05:48:13 +0300

    Хотфикс эксклуда статики и медии

--- src/apps/core/middleware/template_upload_check.py
+++ src/apps/core/middleware/template_upload_check.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 from src.apps.users.models import user_type
 REDIRECT_EXCLUDE_PATHS = ['/auth/logout', '/not-allowed', '/auth/login', '/pupil/templates', '/pupil/upload-template']
+STATIC_EXCLUDE_PATHS = ['/static/', '/media/']
 
 
 class TemplateUploadCheckMiddleware:
@@ -10,8 +11,13 @@ class TemplateUploadCheckMiddleware:
     def __call__(self, request, *args, **kwargs):
         if (request.user.is_authenticated and request.path not in REDIRECT_EXCLUDE_PATHS and
                 request.user.status.id == user_type.PUPIL):
-            user_can_use_site = request.user.is_any_template_uploaded
-            if not user_can_use_site:
-                return redirect('/pupil/templates')
+            check = True
+            for static_path in STATIC_EXCLUDE_PATHS:
+                if static_path in request.path:
+                    check = False
+            if check:
+                user_can_use_site = request.user.is_any_template_uploaded
+                if not user_can_use_site:
+                    return redirect('/pupil/templates')
         response = self._get_response(request)
         return response
